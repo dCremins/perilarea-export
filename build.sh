dcae96cd4441dc542146c448bf783afb2dd46c16
Edge, Matthew <mattedgeis@gmail.com>
2018-03-08 16:15:52 -0500

    Invocation test added

--- build.sh
+++ build.sh
@@ -5,3 +5,12 @@
 GOOS=linux go build main.go
 zip bookie.zip main
 aws lambda update-function-code --function-name perilarea-export --zip-file fileb://./bookie.zip
+rm -f main
+
+# We are passing a request only for the invocation. No input currently required by the function
+aws lambda invoke \
+	--invocation-type RequestResponse \
+	--function-name perilarea-export \
+	--log-type Tail \
+	--payload '{"key1":"value1"}' \
+	/dev/null
